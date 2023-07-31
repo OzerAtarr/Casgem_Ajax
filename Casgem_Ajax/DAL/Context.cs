27c8c4bf15f2e4532647ef135b4e00ec75fc6d23
90536 <90536@DESKTOP-FCHRJ42>
2023-07-31 13:57:31 +0300

    Add project files.

---
+++ Casgem_Ajax/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-FCHRJ42\SQLEXPRESS;initial Catalog=CasgemAjaxDb; integrated security=true;trust server certificate=true;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
